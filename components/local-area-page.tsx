af47767f9ad99e37b2123e28a2670cacd8e75708
wirut <wirutkhatkingpot@gmail.com>
2026-09-21 17:24:54 +0700

    Redesign about page with responsive company timeline

--- components/local-area-page.tsx
+++ components/local-area-page.tsx
@@ -42,7 +42,7 @@ export function LocalAreaPage({ slug }: { slug: LocalAreaSlug }) {
 
   return <div data-legacy-page="services">
     <LegacyBodyState page="services" bodyClass="" />
-    <link rel="stylesheet" href="/legacy/styles.min.css?v=20260921-6" />
+    <link rel="stylesheet" href="/legacy/styles.min.css?v=20260921-8" />
     <link rel="stylesheet" href="/legacy/service-seo.css?v=20260920" />
     <JsonLd data={schema} />
     <a className="skip-link" href="#main-content">ข้ามไปเนื้อหาหลัก</a>
